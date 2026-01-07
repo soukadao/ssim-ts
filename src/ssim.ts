941aefe1ac483075f19098b00e76d78f38d8a8c9
soukadao <s0ukada025@gmail.com>
2026-01-07 14:55:45 +0900

    fix: indent width  4 -> 2

--- src/ssim.ts
+++ src/ssim.ts
@@ -10,13 +10,13 @@ const L = new Decimal(255); // L the dynamic range of the pixel-values
  * @returns
  */
 function sampleMean(x: Uint8ClampedArray): Decimal {
-	let sum = new Decimal(0);
+  let sum = new Decimal(0);
 
-	for (let i = 0; i < x.length; i++) {
-		sum = sum.add(x[i]);
-	}
+  for (let i = 0; i < x.length; i++) {
+    sum = sum.add(x[i]);
+  }
 
-	return sum.div(x.length);
+  return sum.div(x.length);
 }
 
 /**
@@ -25,15 +25,15 @@ function sampleMean(x: Uint8ClampedArray): Decimal {
  * @returns
  */
 function sampleVariance(x: Uint8ClampedArray): Decimal {
-	const pixelSampleMean = sampleMean(x);
-	let sum = new Decimal(0);
+  const pixelSampleMean = sampleMean(x);
+  let sum = new Decimal(0);
 
-	for (let i = 0; i < x.length; i++) {
-		const diff = new Decimal(x[i]).sub(pixelSampleMean);
-		sum = sum.add(diff.pow(2));
-	}
+  for (let i = 0; i < x.length; i++) {
+    const diff = new Decimal(x[i]).sub(pixelSampleMean);
+    sum = sum.add(diff.pow(2));
+  }
 
-	return sum.div(x.length);
+  return sum.div(x.length);
 }
 
 /**
@@ -43,17 +43,17 @@ function sampleVariance(x: Uint8ClampedArray): Decimal {
  * @returns
  */
 function sampleCovariance(x: Uint8ClampedArray, y: Uint8ClampedArray): Decimal {
-	const meanX = sampleMean(x);
-	const meanY = sampleMean(y);
-	let sum = new Decimal(0);
+  const meanX = sampleMean(x);
+  const meanY = sampleMean(y);
+  let sum = new Decimal(0);
 
-	for (let i = 0; i < x.length; i++) {
-		const diffX = new Decimal(x[i]).sub(meanX);
-		const diffY = new Decimal(y[i]).sub(meanY);
-		sum = sum.add(diffX.mul(diffY));
-	}
+  for (let i = 0; i < x.length; i++) {
+    const diffX = new Decimal(x[i]).sub(meanX);
+    const diffY = new Decimal(y[i]).sub(meanY);
+    sum = sum.add(diffX.mul(diffY));
+  }
 
-	return sum.div(x.length);
+  return sum.div(x.length);
 }
 
 /**
@@ -63,35 +63,35 @@ function sampleCovariance(x: Uint8ClampedArray, y: Uint8ClampedArray): Decimal {
  * @returns SSIM value between -1 and 1 (1 means identical)
  */
 export function ssim(x: ImageData, y: ImageData): number {
-	if (x.width !== y.width || x.height !== y.height) {
-		throw new Error("Images must have the same dimensions");
-	}
+  if (x.width !== y.width || x.height !== y.height) {
+    throw new Error("Images must have the same dimensions");
+  }
 
-	const c1 = K1.mul(L).pow(2);
-	const c2 = K2.mul(L).pow(2);
+  const c1 = K1.mul(L).pow(2);
+  const c2 = K2.mul(L).pow(2);
 
-	const xData = x.data;
-	const yData = y.data;
+  const xData = x.data;
+  const yData = y.data;
 
-	const meanX = sampleMean(xData);
-	const meanY = sampleMean(yData);
-	const varianceX = sampleVariance(xData);
-	const varianceY = sampleVariance(yData);
-	const covarianceXY = sampleCovariance(xData, yData);
+  const meanX = sampleMean(xData);
+  const meanY = sampleMean(yData);
+  const varianceX = sampleVariance(xData);
+  const varianceY = sampleVariance(yData);
+  const covarianceXY = sampleCovariance(xData, yData);
 
-	const numerator1 = meanX.mul(meanY).mul(2).add(c1);
-	const denominator1 = meanX.pow(2).add(meanY.pow(2)).add(c1);
-	const luminance = numerator1.div(denominator1);
+  const numerator1 = meanX.mul(meanY).mul(2).add(c1);
+  const denominator1 = meanX.pow(2).add(meanY.pow(2)).add(c1);
+  const luminance = numerator1.div(denominator1);
 
-	const sqrtVarianceX = varianceX.sqrt();
-	const sqrtVarianceY = varianceY.sqrt();
-	const numerator2 = sqrtVarianceX.mul(sqrtVarianceY).mul(2).add(c2);
-	const denominator2 = varianceX.add(varianceY).add(c2);
-	const contrast = numerator2.div(denominator2);
+  const sqrtVarianceX = varianceX.sqrt();
+  const sqrtVarianceY = varianceY.sqrt();
+  const numerator2 = sqrtVarianceX.mul(sqrtVarianceY).mul(2).add(c2);
+  const denominator2 = varianceX.add(varianceY).add(c2);
+  const contrast = numerator2.div(denominator2);
 
-	const numerator3 = covarianceXY.add(c2.div(2));
-	const denominator3 = sqrtVarianceX.mul(sqrtVarianceY).add(c2.div(2));
-	const structure = numerator3.div(denominator3);
+  const numerator3 = covarianceXY.add(c2.div(2));
+  const denominator3 = sqrtVarianceX.mul(sqrtVarianceY).add(c2.div(2));
+  const structure = numerator3.div(denominator3);
 
-	return luminance.mul(contrast).mul(structure).toNumber();
+  return luminance.mul(contrast).mul(structure).toNumber();
 }
